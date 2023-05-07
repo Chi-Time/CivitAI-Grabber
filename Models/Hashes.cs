b0d619921c12a6a98bba918a7f884a7ba2bb4392
James Johnson <james_aaron_johnson@outlook.com>
2023-05-07 23:14:16 +0100

    Fix licence date

--- Models/Hashes.cs
+++ Models/Hashes.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2022 James Johnson
+﻿// Copyright (c) 2023 James Johnson
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
