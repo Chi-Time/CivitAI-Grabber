7fa5a40d6b7f364692669bc44dc95c443bd15a6c
James Johnson <james_aaron_johnson@outlook.com>
2023-05-07 22:58:36 +0100

    Implement main program flow

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿using System.Text;
+﻿using CivitAI_Grabber.Models;
+using System.Text;
 
 namespace CivitAI_Grabber
 {
     internal class Program
     {
         private static readonly NLog.Logger _Logger = NLog.LogManager.GetCurrentClassLogger ();
+        private static readonly List<(DownloadResult Result, string Url)> _Results = new ();
 
         static void Main (string[] args)
         {
             InitialiseNlog ();
+
             var config = Config.Load (@"config.json");
             if (config.IsValid () == false)
             {
@@ -23,12 +26,54 @@ namespace CivitAI_Grabber
                 Terminate ();
             }
 
+            foreach (var link in links)
+            {
+                if (link.ShouldSkip)
+                    continue;
+
+                string modelId = link.Url.Replace ("https://civitai.com/models/", "");
+                modelId = modelId.Split ("/")[0];
+
+                var json = WebUtility.GetJSON ("https://civitai.com/api/v1/models/" + modelId);
+                if (string.IsNullOrWhiteSpace (json))
+                    Terminate ();
+
+                var model = Model.Deserialise (json);
+                if (model == null)
+                    Terminate ();
+
+                model!.Description = WebUtility.RemoveHTML (model.Description);
+
+                Console.WriteLine ($"Downloading: {model.Name} - From Url: {link.Url}");
+                DownloadResult result = Downloader.DownloadModel (model, link, config);
+                _Results.Add ((result, link.Url));
+
+                switch (result)
+                {
+                    case DownloadResult.Success:
+                        Console.WriteLine ("Model File Downloaded.");
+                        break;
+                    case DownloadResult.Failed:
+                        Console.WriteLine ("Model File Failed.");
+                        break;
+                    case DownloadResult.Exists:
+                        Console.WriteLine ("Model File Already Saved.");
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine ($"{model.Name} Downloaded.\n");
+            }
+
+            WriteResults ();
             Console.ReadKey ();
         }
 
         private static void InitialiseNlog ()
         {
-            const string messageFormat = "[${level:uppercase=true}][${longdate}] ${logger}\n${message:exceptionSeparator=\n:withexception=true}";
+            const string seperator = "\n====================\n";
+            const string messageFormat = seperator + "[${level:uppercase=true}]\n[${longdate}] ${logger}\n${message:exceptionSeparator=\n:withexception=true}" + seperator;
             var config = new NLog.Config.LoggingConfiguration ();
             var logFile = new NLog.Targets.FileTarget ("logfile")
             {
@@ -45,6 +90,28 @@ namespace CivitAI_Grabber
             config.AddRule (NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logConsole);
             config.AddRule (NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logFile);
             NLog.LogManager.Configuration = config;
+        }
+
+        private static void WriteResults ()
+        {
+            var failures = _Results.FindAll (x => x.Result == DownloadResult.Failed);
+            var sucesses = _Results.FindAll (x => x.Result == DownloadResult.Success);
+            var existers = _Results.FindAll (x => x.Result == DownloadResult.Exists);
+
+            StringBuilder message = new StringBuilder ();
+            foreach (var failure in failures)
+                message.AppendLine ($"\tModel file: {failure.Url} failed");
+            _Logger.Info ("Failures:\n" + message.ToString ());
+
+            message.Clear ();
+            foreach (var success in sucesses)
+                message.Append ($"\tModel file: {success.Url} downloaded.");
+            _Logger.Info ("Successes:\n" + message.ToString ());
+
+            message.Clear ();
+            foreach (var exists in existers)
+                message.Append ($"\tModel file: {exists.Url} already exists.");
+            _Logger.Info ("Exists:\n" + message.ToString ());
         }
 
         public static void Terminate ()
